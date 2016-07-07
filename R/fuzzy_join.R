b4b18dc62950ee071545dc7c3cf9eb7f5d755d0f
dgrtwo <dgrtwo@princeton.edu>
2016-07-07 16:31:53 -0400

    Added drop = TRUE, which for data.frames (rather than tbl_dfs) allows single column data frames as input. Fixes #13.

--- R/fuzzy_join.R
+++ R/fuzzy_join.R
@@ -250,7 +250,8 @@ fuzzy_join <- function(x, y, by = NULL, match_fun = NULL,
       dplyr::full_join(dplyr::data_frame(y = seq_len(nrow(y))), by = "y")
   }
 
-  ret <- dplyr::bind_cols(x[matches$x, ], y[matches$y, ])
+  ret <- dplyr::bind_cols(x[matches$x, ],
+                          y[matches$y, ])
   if (ncol(matches) > 2) {
     extra_cols <- matches[, -(1:2), drop = FALSE]
     ret <- dplyr::bind_cols(ret, extra_cols)
